1fc929a458432dc5d11f135b3409e4d9500cd73c
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-22 20:53:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,14 @@
 class Person
-  def name = (name)
+  def name(name)
     @name = name
   end
   def name
     @name
   end
-  def job = (job)
+  def job(job)
     @job = job
   end
   def job
     @job
   end
-end
 end
